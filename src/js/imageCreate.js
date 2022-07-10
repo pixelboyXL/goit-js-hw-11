533d6dd7b6995578fbea6749c175540d8cf95f3a
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2022-07-10 15:34:22 +0300

    ver. 2.1

--- src/js/imageCreate.js
+++ src/js/imageCreate.js
@@ -1,45 +1,21 @@
-// export function imageCreate(images) {
-//     return images.map((image) =>
-//         `<div class="gallery--card">
-//             <a href="${image.largeImageURL}">
-//               <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"
-//               class="gallery--image" />
-//             </a>
-//             <div class="galery--info">
-//                 <p class="galery--item">${image.likes}
-//                 <b>Likes</b>
-//                 </p>
-//                 <p class="galery--item">${image.views}
-//                 <b>Views</b>
-//                 </p>
-//                 <p class="galery--item">${image.comments}
-//                 <b>Comments</b>
-//                 </p>
-//                 <p class="galery--item">${image.downloads}
-//                 <b>Downloads</b>
-//                 </p>
-//             </div>
-//         </div>`).join("");
-// };
-
 export function imageCreate(images) {
     return images.map((image) => 
-        `<div class="photo-card">
+        `<div class="gallery--card">
             <a href="${image.largeImageURL}">
               <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"
-              class="photo-ref" />
+              class="gallery--image" />
             </a>
-            <div class="info">
-                <p class="info-item">${image.likes}
+            <div class="galery--info">
+                <p class="galery--item">${image.likes}
                 <b>Likes</b>
                 </p>
-                <p class="info-item">${image.views}
+                <p class="galery--item">${image.views}
                 <b>Views</b>
                 </p>
-                <p class="info-item">${image.comments}
+                <p class="galery--item">${image.comments}
                 <b>Comments</b>
                 </p>
-                <p class="info-item shrink">${image.downloads}
+                <p class="galery--item">${image.downloads}
                 <b>Downloads</b>
                 </p>
             </div>
